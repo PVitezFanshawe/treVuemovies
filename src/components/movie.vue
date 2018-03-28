c182a6616d14db68a746966da7a1081e1a20e72d
Vitez <p_vitez@tss-hr420-dm28.local>
2018-03-28 11:30:24 -0400

    init commit

---
+++ src/components/movie.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ moviesroute }} {{$route.params.id}} </h1>
+</template>
+<script>
+
+export default {
+  name: 'movie' ,
+  data() {
+      return {
+          moviesroute : "this is the movie route for",
+      }
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/scripts/config.js
@@ -0,0 +1,8 @@
+var config = {
+    user : "root",
+    password : "root", // leave this blank for windows machines
+    port: 8888,
+    host: "localhost",
+    database: "db_movies"
+}
+modules.export = config; 
